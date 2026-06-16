962f89c4b67cce671fbd627efdc07af4cfecd00c
sva <chanvanchho987@gmail.com>
2026-06-16 21:14:54 +0700

    u

---
+++ scene_main/Controller.java
@@ -0,0 +1,34 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label label_data;
+
+    @FXML
+    void goto_input(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goto_output(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        
+
+    }
+
+}
