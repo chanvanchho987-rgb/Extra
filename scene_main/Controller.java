21dea53d5aa62fe9539b0a32385ecebb0ddc11a8
sva <chanvanchho987@gmail.com>
2026-06-22 14:04:48 +0700

    u

--- scene_main/Controller.java
+++ scene_main/Controller.java
@@ -1,10 +1,12 @@
 package scene_main;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class Controller {
 
@@ -18,12 +20,17 @@ public class Controller {
     private Label label_data;
 
     @FXML
-    void goto_input(ActionEvent event) {
+    void goto_input(ActionEvent event) throws IOException {
+        // Implementation for navigating to input scene
 
     }
 
     @FXML
-    void goto_output(ActionEvent event) {
+    void goto_output(ActionEvent event) throws IOException {
+        // Implementation for navigating to output scene
+        var stage = (Stage) label_data.getScene().getWindow();
+        var view_output = getClass().getResource("../scene_output/View.fxml");
+        var controller_output = new scene_output.Controller();
 
     }
 
